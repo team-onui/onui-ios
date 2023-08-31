264bf673172327c578ff06a1edc5d02004d19253
hsj._.06 <gtw030488@gmail.com>
2023-08-31 11:10:44 +0900

    init :: project init

---
+++ ONUI-iOS/ONUI-iOS/ONUI_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ONUI_iOSApp.swift
+//  ONUI-iOS
+//
+//  Created by 홍승재 on 2023/08/31.
+//
+
+import SwiftUI
+
+@main
+struct ONUI_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
