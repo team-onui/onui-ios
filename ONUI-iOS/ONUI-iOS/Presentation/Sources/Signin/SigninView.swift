e5f714dbe3a1344ee05f1f55c3f330c2c64cceaa
hsj._.06 <gtw030488@gmail.com>
2023-09-27 10:55:45 +0900

    feat :: file change

---
+++ ONUI-iOS/ONUI-iOS/Presentation/Sources/Signin/SigninView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  ONUI-iOS
+//
+//  Created by 홍승재 on 2023/08/31.
+//
+
+import SwiftUI
+
+struct SigninView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SigninView_Previews: PreviewProvider {
+    static var previews: some View {
+        SigninView()
+    }
+}
