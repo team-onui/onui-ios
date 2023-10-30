e07b878b176ea0f7d49e0e0cc340b29644e01ed7
HongSJae <gtw030488@gmail.com>
2023-10-30 09:42:57 +0900

    feat :: Roboto Font

--- ONUI-iOS/ONUI-iOS/DesignSystem/Source/Typography/Roboto.swift
+++ ONUI-iOS/ONUI-iOS/DesignSystem/Source/Typography/Roboto.swift
@@ -2,22 +2,27 @@ import Foundation
 import SwiftUI
 
 public struct Roboto {
-     public static func registerFonts() {
+    public static func fontHeight(weight: OnuiFontWeight, size: CGFloat) -> CGFloat {
+        UIFont(name: "Roboto-\(weight)", size: size)?.lineHeight ??
+        UIFont.preferredFont(forTextStyle: .caption1).lineHeight
+    }
+
+    public static func registerFonts() {
         OnuiFontWeight.allCases.forEach {
             registerFont(bundle: .main, fontName: "Roboto-\($0.rawValue)", fontExtension: "ttf")
         }
-     }
-
+    }
+    
     private static func registerFont(bundle: Bundle, fontName: String, fontExtension: String) {
-
+        
         guard let fontURL = bundle.url(forResource: fontName, withExtension: fontExtension),
               let fontDataProvider = CGDataProvider(url: fontURL as CFURL),
               let font = CGFont(fontDataProvider) else {
-                  fatalError("Couldn't create font from data")
+            fatalError("Couldn't create font from data")
         }
-
+        
         var error: Unmanaged<CFError>?
-
+        
         CTFontManagerRegisterGraphicsFont(font, &error)
     }
 }
