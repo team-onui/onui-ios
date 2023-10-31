376d8f37bc4c5a3714bad7b7010509de1f681908
HongSJae <gtw030488@gmail.com>
2023-10-31 20:23:46 +0900

    feat :: Onui Base API

---
+++ ONUI-iOS/ONUI-iOS/Data/DataSource/API/OnuiAPI.swift
@@ -0,0 +1,43 @@
+import Foundation
+import Moya 
+
+public protocol OnuiAPI: TargetType, JwtAuthorizable {
+    associatedtype ErrorType: Error
+    var domain: JobisDomain { get }
+    var urlPath: String { get }
+    var errorMap: [Int: ErrorType] { get }
+}
+
+public extension OnuiAPI {
+    var baseURL: URL {
+        URL(
+            string: Bundle.main.object(forInfoDictionaryKey: "BASE_URL") as? String ?? ""
+        ) ?? URL(string: "https://www.google.com")!
+    }
+
+    var path: String {
+        domain.asURLString + urlPath
+    }
+
+    var headers: [String: String]? {
+        ["Content-Type": "application/json"]
+    }
+
+    var validationType: ValidationType {
+        return .successCodes
+    }
+}
+
+public enum JobisDomain: String {
+    case auth
+    case user
+    case image = "img"
+    case diary
+    case timeline = "tl"
+}
+
+extension JobisDomain {
+    var asURLString: String {
+        "/\(self.rawValue)"
+    }
+}
