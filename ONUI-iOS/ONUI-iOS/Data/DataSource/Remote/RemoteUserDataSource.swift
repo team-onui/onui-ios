c76da9a7084ecb277a0abaed6a2952e36d06d2ae
HongSJae <gtw030488@gmail.com>
2023-11-01 08:26:42 +0900

    feat :: User DataSource

---
+++ ONUI-iOS/ONUI-iOS/Data/DataSource/Remote/RemoteUserDataSource.swift
@@ -0,0 +1,13 @@
+import Combine
+
+protocol RemoteUserDataSource {
+    func rename(req: RenameRequestQuery) -> AnyPublisher<String, Error>
+}
+
+final class RemoteUserDataSourceImpl: BaseRemoteDataSource<UserAPI>, RemoteUserDataSource {
+    func rename(req: RenameRequestQuery) -> AnyPublisher<String, Error> {
+        request(.rename(req), dto: RenameResponseDTO.self)
+            .map { $0.toDomain() }
+            .eraseToAnyPublisher()
+    }
+}
