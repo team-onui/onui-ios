5b5dbea2d33627590aacd442b1cd3a0f0688f39e
HongSJae <gtw030488@gmail.com>
2023-11-01 09:45:11 +0900

    feat :: RemoteDiaryDataSource

---
+++ ONUI-iOS/ONUI-iOS/Data/DataSource/Remote/RemoteDiaryDataSource.swift
@@ -0,0 +1,34 @@
+import Combine
+
+protocol RemoteDiaryDataSource {
+    func writeDiary(req: WriteDiaryRequestQuery) -> AnyPublisher<DiaryEntity, Error>
+    func fetchMoodOfMonth(year: Int, month: Int) -> AnyPublisher<[ShortDiaryEntity], Error>
+    func fetchDiaryDetail(id: String) -> AnyPublisher<DiaryDetailEntity, Error>
+    func putDiary(req: PutDiaryRequestQuery) -> AnyPublisher<DiaryEntity, Error>
+}
+
+final class RemoteDiaryDataSourceImpl: BaseRemoteDataSource<DiaryAPI>, RemoteDiaryDataSource {
+    func writeDiary(req: WriteDiaryRequestQuery) -> AnyPublisher<DiaryEntity, Error> {
+        request(.writeDiary(req), dto: DiaryResponseDTO.self)
+            .map { $0.toDomain() }
+            .eraseToAnyPublisher()
+    }
+
+    func fetchMoodOfMonth(year: Int, month: Int) -> AnyPublisher<[ShortDiaryEntity], Error> {
+        request(.fetchMoodOfMonth(year: year, month: month), dto: FetchMoodOfMonthResponseDTO.self)
+            .map { $0.toDomain() }
+            .eraseToAnyPublisher()
+    }
+
+    func fetchDiaryDetail(id: String) -> AnyPublisher<DiaryDetailEntity, Error> {
+        request(.fetchDiaryDetail(id: id), dto: FetchDiaryDetailResponseDTO.self)
+            .map { $0.toDomain() }
+            .eraseToAnyPublisher()
+    }
+
+    func putDiary(req: PutDiaryRequestQuery) -> AnyPublisher<DiaryEntity, Error> {
+        request(.putDiary(req), dto: DiaryResponseDTO.self)
+            .map { $0.toDomain() }
+            .eraseToAnyPublisher()
+    }
+}
