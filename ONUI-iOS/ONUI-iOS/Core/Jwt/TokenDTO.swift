371a40ed66585a6e76887f309e37c890d008f28a
HongSJae <gtw030488@gmail.com>
2023-11-09 23:25:53 +0900

    feat :: TokenDTO 수정

--- ONUI-iOS/ONUI-iOS/Core/Jwt/TokenDTO.swift
+++ ONUI-iOS/ONUI-iOS/Core/Jwt/TokenDTO.swift
@@ -2,14 +2,14 @@ import Foundation
 
 public struct TokenDTO: Equatable, Decodable {
     let accessToken: String
-    let refreshToken: String
     let accessExpiredAt: String
+    let refreshToken: String
     let refreshExpiredAt: String
 
     enum CodingKeys: String, CodingKey {
         case accessToken = "access_token"
-        case refreshToken = "refresh_token"
         case accessExpiredAt = "access_expired_at"
+        case refreshToken = "refresh_token"
         case refreshExpiredAt = "refresh_expired_at"
     }
 }
