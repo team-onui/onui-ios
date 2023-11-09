8b0f140cf82a7a5b7cf870b2fbfeb2dd967ed2fc
HongSJae <gtw030488@gmail.com>
2023-11-09 23:23:03 +0900

    feat :: Google Signin

--- ONUI-iOS/ONUI-iOS/Presentation/Sources/Signin/SigninViewModel.swift
+++ ONUI-iOS/ONUI-iOS/Presentation/Sources/Signin/SigninViewModel.swift
@@ -1,4 +1,27 @@
 import Combine
+import GoogleSignIn
 
 final class SigninViewModel: BaseViewModel {
+    @Published var isNavigatedToMain: Bool = false
+
+    private let googleSigninUseCase: GoogleSigninUseCase
+
+    init(googleSigninUseCase: GoogleSigninUseCase) {
+        self.googleSigninUseCase = googleSigninUseCase
+    }
+
+    func googleSigninButtonDidTap() {
+        guard let vc = (UIApplication.shared.connectedScenes.first as? UIWindowScene)?.windows.first?.rootViewController else { return }
+        GIDSignIn.sharedInstance.signIn(withPresenting: vc) { [self] user, error in
+            guard error == nil else { return }
+            guard let accessToken = user?.user.accessToken.tokenString else { return }
+            googleSignin(accessToken: accessToken)
+        }
+    }
+
+    private func googleSignin(accessToken: String) {
+        addCancellable(googleSigninUseCase.execute(token: accessToken)) { [weak self] _ in
+            self?.isNavigatedToMain.toggle()
+        }
+    }
 }
