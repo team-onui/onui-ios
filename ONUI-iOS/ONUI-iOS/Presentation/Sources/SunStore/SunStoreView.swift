ea14ea9465b3ca09cda73185958ae4933f08912e
hsj._.06 <gtw030488@gmail.com>
2023-11-18 10:18:42 +0900

    Feat :: 햇님 방앗간 API연동

--- ONUI-iOS/ONUI-iOS/Presentation/Sources/SunStore/SunStoreView.swift
+++ ONUI-iOS/ONUI-iOS/Presentation/Sources/SunStore/SunStoreView.swift
@@ -10,8 +10,24 @@ struct SunStoreView: View {
     }
     var body: some View {
         ScrollView {
+            HStack(spacing: 8) {
+                OnuiImage(.chatProfile)
+                    .frame(48)
+
+                Text("남은 쌀은 \(viewModel.rice)개에요..!!")
+                    .onuiFont(.body(.medium), color: .GrayScale.Surface.onSurface)
+                    .padding(.vertical, 4)
+                    .padding(.horizontal, 8)
+                    .background(Color.GrayScale.Surface.surface)
+                    .cornerRadius(16)
+
+                Spacer()
+            }
+            .padding(.horizontal, 8)
+            .padding(.top, 12)
+
             VStack {
-                ForEach(Theme.allCases, id: \.self) { theme in
+                ForEach(viewModel.themeList, id: \.theme) { theme in
                     themeCell(theme: theme)
                 }
                 
@@ -20,6 +36,7 @@ struct SunStoreView: View {
             .padding(.vertical, 12)
             .padding(.horizontal, 16)
         }
+        .onAppear(perform: viewModel.onAppear)
         .background(Color.GrayScale.Background.background)
         .setBackButton(title: "햇님 방앗간") {
             dismiss()
@@ -27,51 +44,68 @@ struct SunStoreView: View {
     }
 
     @ViewBuilder
-    func themeCell(theme: Theme) -> some View {
-        VStack(spacing: 8) {
-            Text(theme.toKorean())
+    func themeCell(theme: ThemeEntity) -> some View {
+        VStack(alignment: .trailing, spacing: 8) {
+            Text(theme.theme.toKorean())
                 .onuiFont(.title(.medium), color: .black)
-
-            VStack(alignment: .trailing, spacing: 8) {
-                HStack {
-                    ForEach(theme.toImages().reversed(), id: \.self) { image in
-                        image.frame(56)
-
-                        if image != theme.toImages().last {
-                            Spacer()
-                        }
+                .frame(maxWidth: .infinity)
+            
+            HStack {
+                ForEach(theme.theme.toImages().reversed(), id: \.self) { image in
+                    image
+                        .frame(56)
+                    
+                    if image != theme.theme.toImages().last {
+                        Spacer()
                     }
-                }
-
-                Button {
-                    viewModel.changeTheme(theme: theme)
-                    dismiss()
-                    var themeType: ThemeType {
-                        switch theme {
-                        case .standard:
-                            return .standard
-                        case .hong:
-                            return .hong
-                        case .ssac:
-                            return .ssac
-                        case .nya:
-                            return .nya
-                        }
-                    }
-                    appState.theme = themeType.toDomain()
-                } label: {
-                    Text("적용하기")
-                        .onuiFont(.body(.medium), color: .Primary.onPrimaryContainer)
-                        .padding(.vertical, 4)
-                        .padding(.horizontal, 8)
-                        .background(Color.Primary.primaryContainer)
-                        .cornerRadius(12)
                 }
             }
-            .padding(.vertical, 12)
-            .padding(.horizontal, 16)
-            .background(Color.GrayScale.Surface.surface)
-            .cornerRadius(24)
+            
+            Button {
+                viewModel.buyTheme(theme: theme)
+            } label: {
+                Text(theme.isBought ? "구매완료": "\(theme.price)쌀")
+                    .onuiFont(.body(.medium), color: theme.isBought ? .Primary.primary :.Primary.onPrimaryContainer)
+                    .padding(.vertical, 4)
+                    .padding(.horizontal, 8)
+                    .background(theme.isBought ? .clear: Color.Primary.primaryContainer)
+                    .cornerRadius(12)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 12)
+                            .stroke(Color.Primary.primary, lineWidth: 1)
+                            .opacity(theme.isBought ? 1: 0)
+                    }
+            }
+            .disabled(theme.isBought)
+        }
+        .padding(.vertical, 12)
+        .padding(.horizontal, 16)
+        .background(Color.GrayScale.Surface.surface)
+        .cornerRadius(24)
+        .onuiAlert(
+            isPresented: $viewModel.isShowAlert,
+            title: "테마 즉시 적용",
+            content:
+"""
+홍조쓰 테마를 바로 적용하시겠습니까?
+보유 테마는 [설정 > 내정보] 아래에서 확인 가능해요!
+"""
+        ) {
+            viewModel.changeTheme(completion: { theme in
+                var themeType: ThemeType {
+                    switch theme {
+                    case .standard:
+                        return .standard
+                    case .hong:
+                        return .hong
+                    case .ssac:
+                        return .ssac
+                    case .nya:
+                        return .nya
+                    }
+                }
+                appState.theme = themeType.toDomain()
+            })
         }
     }
 }
--- ONUI-iOS/ONUI-iOS/Presentation/Sources/SunStore/SunStoreViewModel.swift
+++ ONUI-iOS/ONUI-iOS/Presentation/Sources/SunStore/SunStoreViewModel.swift
@@ -1,15 +1,50 @@
 import Foundation
 
 final class SunStoreViewModel: BaseViewModel {
-    private let changeThemeUseCase: ChangeThemeUseCase
+    @Published var rice: Int = 0
+    @Published var isSuccessChangeTheme = false
+    @Published var isShowAlert = false
+    @Published var themeList = [ThemeEntity]()
+    @Published var currentBoughtTheme: ThemeEntity?
 
-    init(changeThemeUseCase: ChangeThemeUseCase) {
+    private let changeThemeUseCase: ChangeThemeUseCase
+    private let buyThemeUseCase: BuyThemeUseCase
+    private let fetchMyRiceUseCase: FetchMyRiceUseCase
+    private let fetchThemeListUseCase: FetchThemeListUseCase
+
+    init(
+        changeThemeUseCase: ChangeThemeUseCase,
+        buyThemeUseCase: BuyThemeUseCase,
+        fetchMyRiceUseCase: FetchMyRiceUseCase,
+        fetchThemeListUseCase: FetchThemeListUseCase
+    ) {
         self.changeThemeUseCase = changeThemeUseCase
+        self.buyThemeUseCase = buyThemeUseCase
+        self.fetchMyRiceUseCase = fetchMyRiceUseCase
+        self.fetchThemeListUseCase = fetchThemeListUseCase
     }
 
-    func changeTheme(theme: Theme) {
+    func onAppear() {
+        fetchMyRice()
+        fetchThemeList()
+    }
+
+    private func fetchMyRice() {
+        addCancellable(fetchMyRiceUseCase.execute()) { rice in
+            self.rice = rice
+        }
+    }
+
+    private func fetchThemeList() {
+        addCancellable(fetchThemeListUseCase.execute()) { themeList in
+            self.themeList = themeList.filter { $0.theme != .standard}
+        }
+    }
+
+    func changeTheme(completion: @escaping (Theme) -> Void) {
+        guard let currentBoughtTheme else { return }
         var themeType: ThemeType {
-            switch theme {
+            switch currentBoughtTheme.theme {
             case .standard:
                 return .standard
             case .hong:
@@ -20,6 +55,29 @@ final class SunStoreViewModel: BaseViewModel {
                 return .nya
             }
         }
-        addCancellable(changeThemeUseCase.execute(theme: themeType)) { _ in }
+        addCancellable(changeThemeUseCase.execute(theme: themeType)) { _ in
+            completion(currentBoughtTheme.theme)
+        }
+    }
+
+    func buyTheme(theme: ThemeEntity) {
+        var themeType: ThemeType {
+            switch theme.theme {
+            case .standard:
+                return .standard
+            case .hong:
+                return .hong
+            case .ssac:
+                return .ssac
+            case .nya:
+                return .nya
+            }
+        }
+        addCancellable(buyThemeUseCase.execute(theme: themeType)) { themeList in
+            self.rice -= theme.price
+            self.themeList = themeList.filter { $0.theme != .standard}
+            self.isShowAlert = true
+            self.currentBoughtTheme = theme
+        }
     }
 }
