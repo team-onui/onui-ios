92b8689bbb1901f0588bf8479ec8f8577a8b1b1d
hsj._.06 <gtw030488@gmail.com>
2023-11-19 23:22:26 +0900

    feat :: MoonStoreView

---
+++ ONUI-iOS/ONUI-iOS/Presentation/Sources/MoonStore/MoonStoreView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct MoonStoreView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        VStack {
+            Text("추후 추가될 예정입니다 🧑‍💻")
+                .onuiFont(.body(.medium), color: .GrayScale.Surface.onSurface)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.GrayScale.Background.background)
+        .setBackButton(title: "햇님 방앗간") {
+            dismiss()
+        }
+    }
+}
